c552c3ede73aa855739cd76d8976a752fcaf07be
Allen Bargi <allen.bargi@gmail.com>
2011-01-23 10:10:43 +0100

    added linux-server.sh to the templates

---
+++ templates/linux/linux-server.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+sudo apt-get -y install curl && curl https://github.com/joshfng/railsready/raw/master/railsready.sh >> railsready.sh && chmod a+x railsready.sh && ./railsready.sh
+
+# TODO: 
+# 1. dotfiles
+# 2. dotfiles requirements
+# 3. node.js
+# 4. ssh server
+# 5. php
+# 6. Apache
+# 7. Nginx
+# 8. MySQL
+# 9. MongoDB
+# 10. sqlite and mysql ruby gems
+# 11. Tools: sudo apt-get -y install vim tmux ack htop dialog curl zip unzip
+
