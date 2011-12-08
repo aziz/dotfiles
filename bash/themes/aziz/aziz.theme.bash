52f59ca5d45a3f34885da3359dabb9b2fbb3c8dd
Allen Bargi <allen.bargi@gmail.com>
2011-12-08 14:58:48 +0100

    minor changes to bash theme

--- bash/themes/aziz/aziz.theme.bash
+++ bash/themes/aziz/aziz.theme.bash
@@ -4,7 +4,7 @@ SCM_THEME_PROMPT_CLEAN=" ${bold_green}✔"
 SCM_THEME_PROMPT_PREFIX=" "
 SCM_THEME_PROMPT_SUFFIX=" "
 
-PROMPT="\[${bold_yellow}\]\w\[\$(rvmrc_version_prompt)\]\[${bold_blue}\]\[\$(scm_char)\]\[\$(scm_prompt_info)\]\[${normal}\]\[${reset_color}\] → "
+PROMPT="\[${bold_yellow}\]\w \[\$(rvmrc_version_prompt)\]\[${bold_blue}\]\[\$(scm_char)\]\[\$(scm_prompt_info)\]\[${normal}\]\[${reset_color}\]→ "
 
 # git theming
 GIT_THEME_PROMPT_DIRTY=" ${bold_red}✖"
@@ -12,7 +12,7 @@ GIT_THEME_PROMPT_CLEAN=" ${bold_green}✔"
 GIT_THEME_PROMPT_PREFIX=""
 GIT_THEME_PROMPT_SUFFIX=""
 
-RVM_THEME_PROMPT_PREFIX=" ${red}✦ "
+RVM_THEME_PROMPT_PREFIX="${red}✦ "
 RVM_THEME_PROMPT_SUFFIX=" ${reset_color}"
 
 SCM_NONE_CHAR=""
