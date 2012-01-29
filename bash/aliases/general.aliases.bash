99f7218f59a36931e0c3598b656ed43abec9f640
Allen Bargi <allen.bargi@gmail.com>
2012-01-29 17:52:45 +0100

    adding ls++ conf file and alias

--- bash/aliases/general.aliases.bash
+++ bash/aliases/general.aliases.bash
@@ -10,10 +10,11 @@ alias reload='source ~/.bash_profile && echo "bash_profile reloaded"'
 alias sl=ls
 alias ls='ls -G'        # Compact view, show colors
 alias la='ls -AF'       # Compact view, show hidden
-alias ll='ls -al'
+alias ll='ls -alh'
 alias l='ls -a'
 alias l1='ls -1'
 alias lo='ls -l | sed -e 's/--x/1/g' -e 's/-w-/2/g' -e 's/-wx/3/g' -e 's/r--/4/g' -e 's/r-x/5/g' -e 's/rw-/6/g' -e 's/rwx/7/g' -e 's/---/0/g''  # convert permissions to octal
+alias lss='ls++'
 
 alias recent='ls -lAt | head'
 alias old='ls -lAt | tail'
