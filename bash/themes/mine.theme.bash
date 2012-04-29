2617262a268f3057c0278e72a7f2230c1e12ab1a
Allen Bargi <allen.bargi@gmail.com>
2012-04-29 20:10:56 +0200

    going to column zero to fix some old terminal issues

--- bash/themes/mine.theme.bash
+++ bash/themes/mine.theme.bash
@@ -19,4 +19,5 @@ rvmrc_version_prompt () {
   fi
 }
 
-PROMPT="\`${EXIT_STATUS}\`\[${bold_yellow}\]\w \[\$(rvmrc_version_prompt)\]\[${bold_blue}\]\[\$(scm_char)\]\[\$(scm_prompt_info)\]\[${normal}\]\[${reset_color}\]→ "
+# \[\033[G\]: goto first col
+PROMPT="\[\033[G\]\`${EXIT_STATUS}\`\[${bold_yellow}\]\w \[\$(rvmrc_version_prompt)\]\[${bold_blue}\]\[\$(scm_char)\]\[\$(scm_prompt_info)\]\[${normal}\]\[${reset_color}\]→ "
