71cb10f2af09d2dee2052ea9eabe067ab8c52705
Allen Bargi <allen.bargi@gmail.com>
2013-03-28 12:17:25 +0100

    added fix_open_with_duplicated for OSX alias

--- bash/aliases/osx.aliases.bash
+++ bash/aliases/osx.aliases.bash
@@ -3,6 +3,8 @@
 # fix_slow_terminal
 alias fast_terminal='sudo rm -rf /private/var/log/asl/*.asl'
 
+alias fix_open_with_duplicated='/System/Library/Frameworks/CoreServices.framework/Versions/A/Frameworks/LaunchServices.framework/Versions/A/Support/lsregister -kill -r -domain local -domain user && killall Finder && echo "Open With has been rebuilt, Finder will relaunch"'
+
 # Desktop Programs
 alias preview="open -a '$PREVIEW'"
 alias xcode="open -a '/Developer/Applications/Xcode.app'"
@@ -13,3 +15,5 @@ alias chromium="open -a chromium"
 alias f='open -a Finder '
 alias ql='qlmanage -p'
 alias textedit='open -a TextEdit'
+
+
