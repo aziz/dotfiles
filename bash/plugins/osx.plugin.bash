c9b39fff1455990961a3eaac51b89b46a7d3c3cc
Allen Bargi <allen.bargi@gmail.com>
2014-02-07 23:14:05 +0100

    prefix mac specific aliases

--- bash/plugins/osx.plugin.bash
+++ bash/plugins/osx.plugin.bash
@@ -1,21 +1,22 @@
 #!/bin/bash
 
 # TODO: convert to functions with parameters
-alias desktop_hide="defaults write com.apple.finder CreateDesktop -bool false && killall Finder"
-alias desktop_show="defaults write com.apple.finder CreateDesktop -bool true && killall Finder"
-alias spotlight_hide="sudo chmod 600 /System/Library/CoreServices/Search.bundle/Contents/MacOS/Search && killall SystemUIServer"
-alias spotlight_show="sudo chmod 755 /System/Library/CoreServices/Search.bundle/Contents/MacOS/Search && killall SystemUIServer"
+alias mac_desktop_hide="defaults write com.apple.finder CreateDesktop -bool false && killall Finder"
+alias mac_desktop_show="defaults write com.apple.finder CreateDesktop -bool true && killall Finder"
+alias mac_spotlight_hide="sudo chmod 600 /System/Library/CoreServices/Search.bundle/Contents/MacOS/Search && killall SystemUIServer"
+alias mac_spotlight_show="sudo chmod 755 /System/Library/CoreServices/Search.bundle/Contents/MacOS/Search && killall SystemUIServer"
+
 # Disable press-and-hold for keys in favor of key repeat
-alias press_and_hold_disable="defaults write -g ApplePressAndHoldEnabled -bool false"
-alias press_and_hold_enable="defaults write -g ApplePressAndHoldEnabled -bool true"
+alias mac_press_and_hold_disable="defaults write -g ApplePressAndHoldEnabled -bool false"
+alias mac_press_and_hold_enable="defaults write -g ApplePressAndHoldEnabled -bool true"
 
 alias mac_hide="chflags hidden"
 alias mac_show="chflags nohidden"
 
-alias openports='sudo lsof -iTCP -sTCP:LISTEN -P'
+alias mac_openports='sudo lsof -iTCP -sTCP:LISTEN -P'
 
 # Recursively delete `.DS_Store` files
-alias ds_store_cleanup="find . -name '*.DS_Store' -type f -ls -delete"
+alias mac_ds_store_cleanup="find . -name '*.DS_Store' -type f -ls -delete"
 
 # Always open everything in Finder's list view. This is important.
 alias finder_always_use_list_view="defaults write com.apple.Finder FXPreferredViewStyle Nlsv"
