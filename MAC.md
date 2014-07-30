8a7bfc58d91292cfe23b92dfc03ebb08590f06b9
Allen Bargi <allen.bargi@gmail.com>
2014-07-31 06:36:38 +1000

    added more stuff to mac-clean

---
+++ MAC.md
@@ -0,0 +1,68 @@
+- atom
+- textmate
+
+- onepassword
+  - ~/Library/Preferences/com.agilebits.onepassword4.plist
+  - ~/Library/Preferences/2BUA8C4S2C.com.agilebits.onepassword4-helper.plist
+- iterm
+  - ~/Library/Preferences/com.googlecode.iterm2.plist
+  - /Users/allen/Library/Application Support/iTerm
+- transmission
+  - ~/Library/Application Support/Transmission
+  - ~/Library/Preferences/org.m0k.transmission.LSSharedFileList.plist
+  - ~/Library/Preferences/org.m0k.transmission.plist
+- spectacle
+  - ~/Library/Preferences/com.divisiblebyzero.Spectacle.plist
+- skype
+  - ~/Library/Preferences/com.skype.skype.plist
+  - ~/Library/Application Support/Skype
+- gitx
+  - ~/Library/Preferences/net.phere.GitX.LSSharedFileList.plist
+  - ~/Library/Preferences/net.phere.GitX.plist
+- vlc
+  - ~/Library/Preferences/org.videolan.vlc/*
+  - ~/Library/Preferences/org.videolan.vlc.plist
+  - ~/Library/Application Support/VLC
+  - ~/Library/Application Support/org.videolan.vlc
+  - ~/Library/Application Support/org.videolan.vlc
+- mail notifr
+  - ~/Library/Containers/com.ashchan.GmailNotifrHelper/*
+- twitter
+  - ~/Library/Containers/com.twitter.twitter-mac/*
+- palua
+  - ~/Library/Preferences/com.molowa.palua.plist
+- dropbox
+  - /Library/DropboxHelperTools
+  - ~/.dropbox
+- totalfinder
+  - ~/Library/Preferences/com.binaryage.totalfinder.plist
+  - /Library/ScriptingAdditions/TotalFinder.osax
+- bartender
+- little-snitch
+- chrome
+- firefox
+- safari
+- dock 
+  - ~/Library/Preferences/com.apple.dock.plist
+
+- hide desktop
+- disable dashboard
+- disable control+shift system wide command
+- download extra voices
+- default settings for finder
+  - alwayslist
+  - calculate all sizes
+  - size and kind only
+  - sort by name
+
+
+https://github.com/pivotal-sprout
+https://github.com/pivotal-sprout/sprout
+https://github.com/pivotal-sprout/sprout-osx-settings
+https://github.com/pivotal-sprout/sprout-osx-settings/blob/master/recipes/set_multitouch_preferences.rb
+https://github.com/pivotal-sprout/sprout-osx-settings/blob/master/recipes/set_finder_show_user_home_in_sidebar.rb
+https://github.com/pivotal-sprout/sprout-osx-settings/blob/master/recipes/remove_expose_keyboard_shortcuts.rb
+
+caffeinate -dis
+https://gist.github.com/luisbarrueco/9259848
+http://palcu.blogspot.com.au/2014/06/dotfiles-and-dev-tools-provisioned-by.html
--- mac/mac-clean.sh
+++ mac/mac-clean.sh
@@ -1,25 +1,16 @@
 #!/bin/bash
 
-# cleanup (){
-#   # restore sudoers original state
-#   sudo sed -ibkp '/Defaults timestamp_timeout=-1/d' /etc/sudoers
-# }
-
-# echo "==> Making sudo ask for passwords just once"
-# echo "Please, enter your local account's password."
-# echo "Defaults  timestamp_timeout=-1" | sudo tee -a /etc/sudoers || exit 2
-
 # -------------------------------------------------------------
 # Cleaning Log files
 echo "==> cleaning log files"
-sudo rm -Rf $HOME/Library/Logs/*
+sudo rm -Rf "$HOME"/Library/Logs/*
 echo "$HOME/Library/Logs"
 sudo rm -Rf /Library/Logs/*
 echo "/Library/Logs"
 
 # Cleaning Cache files
 # echo "==> cleaning cache files"
-# sudo rm -Rf $HOME/Library/Caches/*
+# sudo rm -Rf "$HOME"/Library/Caches/*
 # echo "$HOME/Library/Caches"
 # sudo rm -Rf /Library/Caches/*
 # echo "/Library/Caches"
@@ -30,16 +21,15 @@ echo "/Library/Logs"
 
 echo "==> cleaning Locale files"
 macCleanList=`mktemp -t macClean`
+echo "searching..."
 sudo find /Users /Applications /opt /Library /System -iregex '.*\.lproj' | \
-perl -wnE'say for /.+(?<!English)(?<!en)(?<!en_US)(?<!Base)\.lproj/g' > $macCleanList
-while read in; do sudo rm -Rf "$in"; done < $macCleanList
+perl -wnE 'say for /.+(?<!English)(?<!en)(?<!en_US)(?<!Base)\.lproj/g' > "$macCleanList"
+echo "found `cat $macCleanList | wc -l`"
+echo "deleting..."
+while read in; do sudo rm -Rf "$in"; done < "$macCleanList"
 
 # -------------------------------------------------------------
-#
 # Chrome
-# ✔ remove old versions extension
-# ✔ clean up old versions of chrome
-# - clean up old versions of chrome canary
 # - save Local Extension Settings
 
 # Clean Chrome Extensions folder, keeping olny the most recent version
@@ -56,44 +46,69 @@ for EDIR in `ls -1 "$EXTS_DIR"`; do
 done
 
 # Clean up old versions of chrome
-chrome=`osascript -e 'POSIX path of (path to application "Chrome")'`
+cleanup_old_versions_of_chrome() {
+  chrome_versions_count=0
+  if [ ! -z "$chrome" ]; then
+    echo "$chrome"
+    chrome_versions_count=$(ls "$chrome"/Contents/Versions | wc -l)
+    if [[ $chrome_versions_count -gt 1 ]]; then
+      echo "more than one version, should be cleaned"
+      ls -1tUr "$chrome/Contents/Versions" | \
+      head -n $(expr $chrome_versions_count - 1) | \
+      awk -v path="$chrome/Contents/Versions/" '{ print "\"" path $0 "\"" }' | \
+      xargs rm -Rf
+    else
+      echo "just one verison"
+    fi
+  else
+    echo "can't find app"
+  fi
+}
+
 echo "==> cleaning old versions of Google Chrome"
-echo $chrome
-chrome_versions_count=`ls "$chrome/Contents/Versions" | wc -l`
-if [ $chrome_versions_count -gt 1 ]; then
-  echo "more than one version, should be cleaned"
-else
-  echo "just one verison"
-fi
+chrome=$(osascript -e 'POSIX path of (path to application "Chrome")' 2> /dev/null)
+cleanup_old_versions_of_chrome
+
+chrome=$(osascript -e 'POSIX path of (path to application "Google Chrome Canary")' 2> /dev/null)
+echo "==> cleaning old versions of Google Chrome Canary"
+cleanup_old_versions_of_chrome
+
+# -------------------------------------------------------------
+source clean-fonts.sh
 
 # -------------------------------------------------------------
 # Listing Launch Agents
-echo "==> LaunchAgents"
-ls -1 $HOME/Library/LaunchAgents
-ls -1 /Library/LaunchAgents
-ls -1 /Library/LaunchDaemons
+# echo "==> LaunchAgents"
+# crontab -l 2> /dev/null
+# ls -1 "$HOME"/Library/LaunchAgents
+# ls -1 /Library/LaunchAgents
+# ls -1 /Library/LaunchDaemons
+# ls -1 /Library/StartupItems
+# osascript -e 'tell application "System Events" to get the name of every login item' | sed -e  $'s/, /\\\n/g'
 
 # -------------------------------------------------------------
 
+
+# Get rid of all recent files
+# LSSharedFileList
+# FXRecentFolders
+# NSNavLastRootDirectory
+# NSNavLastCurrentDirectory
+# NSNavRecentPlaces
+# ~/Library/Preferences/com.apple.recentitems.plist
+# VLC: ml.xspf
 
 # Find Stuff
 # sudo find ~ /Library /System/Library -iregex '.*iterm.*'
 # sudo find /Users /Library /Applications /System /opt /bin /sbin /private  -iregex '.*iterm.*'
 
-# Get rid of all recent files
-# sudo find ~ /Library -iregex '.*LSSharedFileList.*'
-
 # $HOME/Library/Messages
 # $HOME/Library/Saved Application State
 # $HOME/Library/Application Support/CrashReporter
 
-
-# Delete System fonts I dont use /Library/Fonts
 # Delete System dictionaries I dont use /Library/Dictionaries
 # Delete DesktopPicktures
 # Delete Screen Savers
 
 # BACKUP
 # $HOME/Library/Keychains
-
-# cleanup
