3a2d988ebb2001b66decf51f2157c2d63f063cf1
Allen Bargi <allen.bargi@gmail.com>
2015-01-19 21:05:57 +1100

    updated docker ENV vars

--- bash/plugins/docker.plugin.bash
+++ bash/plugins/docker.plugin.bash
@@ -1,5 +1,7 @@
 # Note, this might need to be different if you have an older version
 # of boot2docker, or its configured for a different IP
 if [[ $OS == "darwin" ]]; then
-  export DOCKER_HOST=tcp://192.168.59.103:2375
+  export DOCKER_HOST=tcp://192.168.59.103:2376
+  export DOCKER_CERT_PATH=/Users/allen.bargi/.boot2docker/certs/boot2docker-vm
+  export DOCKER_TLS_VERIFY=1
 fi
