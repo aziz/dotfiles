be2491735d427abfaa766fecb95f10179898945e
Allen Bargi <allen.bargi@gmail.com>
2015-03-13 11:47:44 +1100

    added babel and typescript

--- bash/themes/mine.theme.bash
+++ bash/themes/mine.theme.bash
@@ -7,7 +7,7 @@ SCM_THEME_PROMPT_SUFFIX=" "
 SCM_NONE_CHAR=""
 RVM_THEME_PROMPT_PREFIX="${bold_red}✦ "
 RVM_THEME_PROMPT_SUFFIX=" ${reset_color}"
-ERROR="${bold_red}🚫 Error \n${normal}"
+ERROR="${bold_red}❌  Error \n${normal}"
 EXIT_STATUS="if [ \$? = 0 ]; then echo \"\"; else echo \"${ERROR}\"; fi"
 
 rvmrc_version_prompt () {
