db2ad126bc4607c846c7714d4c1ce67b4cf74fef
Allen Bargi <allen.bargi@gmail.com>
2015-04-18 14:52:44 +1000

    added jenkins to brew list plus some docker helpers

--- bash/plugins/docker.plugin.bash
+++ bash/plugins/docker.plugin.bash
@@ -12,3 +12,15 @@ if [[ $OS == "darwin" ]]; then
 
 fi
 
+# Kill all running containers.
+alias dockerkillall='docker kill $(docker ps -q)'
+
+# Delete all stopped containers.
+alias dockercleanc='printf "\n>>> Deleting stopped containers\n\n" && docker rm $(docker ps -a -q)'
+
+# Delete all untagged images.
+alias dockercleani='printf "\n>>> Deleting untagged images\n\n" && docker rmi $(docker images -q -f dangling=true)'
+
+# Delete all stopped containers and untagged images.
+alias dockerclean='dockercleanc || true && dockercleani'
+
