629470f6d0ee82860eacccf8791e45cb55bff647
Allen Bargi <allen.bargi@gmail.com>
2015-05-04 03:30:06 +1000

    fixed ssh agent issue in linux

--- bash/plugins/ssh.plugin.bash
+++ bash/plugins/ssh.plugin.bash
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 # Starting ssh-agent if it's not already started. fix a problem in MacOS Lion 10.7.x
-if [ "x`ps ax | grep [s]sh-agent`" == "x" ]; then ssh-agent && ssh-add; fi
+if [ "$(uname)" = "Darwin" ]; then
+  if [ "x`ps ax | grep [s]sh-agent`" == "x" ]; then ssh-agent && ssh-add; fi
+fi
 
 ssh_add_server () {
   echo -en "\n\nHost $1\n  HostName $2\n  User $3\n  ServerAliveInterval 30\n  ServerAliveCountMax 120" >> ~/.ssh/config
