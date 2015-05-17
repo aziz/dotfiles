4b969d57e840e4e28836b783b8d526f9f29cd10d
Allen Bargi <allen.bargi@gmail.com>
2015-05-17 21:58:54 +1000

    removed some unused plugins

--- bash/plugins/ruby.plugin.bash
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-remove_gem() {
-  gem list | grep "$1" | awk '{ print $1; }' | xargs sudo gem uninstall
-}
-
-fav_gems_install() {
-  while read in; do gem install "$in"; done < "$HOME"/.templates/dependencies/gem
-}
