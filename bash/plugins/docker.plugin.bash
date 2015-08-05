a305fbe8980b912eb8e3f81732a557566237236b
Allen Bargi <allen.bargi@gmail.com>
2015-08-05 14:19:51 +1000

    Added docker-ip

--- bash/plugins/docker.plugin.bash
+++ bash/plugins/docker.plugin.bash
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 
 if [ "$(uname)" = "Darwin" ]; then
-  # Note, this might need to be different if you have an older version
-  # of boot2docker, or its configured for a different IP
+
   export DOCKER_HOST=tcp://192.168.59.103:2376
-  export DOCKER_CERT_PATH=/Users/allen.bargi/.boot2docker/certs/boot2docker-vm
+  export DOCKER_CERT_PATH=$HOME/.boot2docker/certs/boot2docker-vm
   export DOCKER_TLS_VERIFY=1
 
   docker-enter() {
@@ -26,3 +25,11 @@ alias dockercleani='printf "\n>>> Deleting untagged images\n\n" && docker rmi $(
 # Delete all stopped containers and untagged images.
 alias dockerclean='dockercleanc || true && dockercleani'
 
+# list running docker containers and their ips
+# if you name them meaningfully it'll be nicer
+docker-ip() {
+  for i in $(docker ps -q); do
+    echo -n "$i"
+    docker inspect --format ' {{ .Name }} {{ .NetworkSettings.IPAddress }}' "$i"
+  done
+}
