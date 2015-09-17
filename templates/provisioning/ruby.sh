75f2812d9a58eefb5d50e9ce0e01e228ebc68c57
Allen Bargi <allen.bargi@gmail.com>
2015-09-17 14:34:29 +0200

    using autolibs=homebrew for rvm installation

--- templates/provisioning/ruby.sh
+++ templates/provisioning/ruby.sh
@@ -8,7 +8,7 @@ echo "|_|    \_/ |_| |_| |_|"
 echo "_______________________"
 echo
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
-curl -sSL https://get.rvm.io | bash -s -- --ignore-dotfiles
+curl -sSL https://get.rvm.io | bash -s stable --autolibs=homebrew --ignore-dotfiles
 while read in; do echo "==> Installing Ruby $in";rvm install ruby-"$in"; done < "$HOME"/.templates/dependencies/rubies
 source ~/.bashrc
 rvm use ruby-"$(cat "$HOME"/.templates/dependencies/ruby-default)" --default
