6d68ca555cc3ed09c590617c8d067a0f111c501e
Allen Bargi <allen.bargi@gmail.com>
2015-10-18 14:02:05 +0200

    NODE_REPL_HISTORY_FILE deprecated

--- bash/plugins/node.plugin.bash
+++ bash/plugins/node.plugin.bash
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 export NODE_PATH="/usr/local/lib/node_modules"
-export NODE_REPL_HISTORY_FILE="$HOME/.node_history"
+export NODE_REPL_HISTORY="$HOME/.node_history"
 export NODE_REPL_HISTORY_SIZE='32768'
 
 alias npmg="npm -g"
