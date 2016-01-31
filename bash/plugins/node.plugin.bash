7f359b3f6361520e0f78875e925785d18f5e1ee1
Allen Bargi <allen.bargi@gmail.com>
2016-01-31 22:45:08 +0100

    added NVM config

--- bash/plugins/node.plugin.bash
+++ bash/plugins/node.plugin.bash
@@ -4,6 +4,9 @@ export NODE_PATH="$HOME/.npm-packages/lib/node_modules"
 export NODE_REPL_HISTORY="$HOME/.node_history"
 export NODE_REPL_HISTORY_SIZE='32768'
 
+export NVM_DIR="$HOME/.nvm"
+source $(brew --prefix nvm)/nvm.sh
+
 alias npmg="npm -g"
 alias npmgo="npm -g outdated"
 alias npmgo0='npm -g outdated --depth=0'
