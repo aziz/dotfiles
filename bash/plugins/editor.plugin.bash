874552aa7e53eca0b710a36e373dfe52d64a801e
Allen Bargi <allen.bargi@gmail.com>
2016-02-12 12:06:48 +0100

    added vs-code launch from terminal

--- bash/plugins/editor.plugin.bash
+++ bash/plugins/editor.plugin.bash
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+code() {
+  VSCODE_CWD="$PWD" open -n -b "com.microsoft.VSCode" --args $*;
+}
+
 subl_symlink() {
   if [ "$(uname)" = "Darwin" ]; then
     ST_APP=$(osascript -e 'POSIX path of (path to application "Sublime Text")')
