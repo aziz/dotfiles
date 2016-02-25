fad588eb0b0f9a7617c68a2afed785f3d54a6a6b
Allen Bargi <allen.bargi@gmail.com>
2016-02-25 15:25:37 +0100

    RIP boot2docker

--- bash/plugins/docker.plugin.bash
+++ bash/plugins/docker.plugin.bash
@@ -1,16 +1,7 @@
 #!/usr/bin/env bash
 
 if [ "$(uname)" = "Darwin" ]; then
-
-  export DOCKER_HOST=tcp://192.168.59.103:2376
-  export DOCKER_CERT_PATH=$HOME/.boot2docker/certs/boot2docker-vm
-  export DOCKER_TLS_VERIFY=1
-
-  docker-enter() {
-    boot2docker ssh '[ -f /var/lib/boot2docker/nsenter ] || docker run --rm -v /var/lib/boot2docker/:/target jpetazzo/nsenter'
-    boot2docker ssh -t sudo "/var/lib/boot2docker/docker-enter \"$1\""
-  }
-
+  eval "$(docker-machine env default)"
 fi
 
 # Kill all running containers.
