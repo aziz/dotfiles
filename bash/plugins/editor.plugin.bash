5ee5f8926dd59a35a9b69dad46a30f7938f3971d
Allen Bargi <allen.bargi@gmail.com>
2016-03-10 19:42:37 +0100

    removed old vs-code function

--- bash/plugins/editor.plugin.bash
+++ bash/plugins/editor.plugin.bash
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 
-code() {
-  VSCODE_CWD="$PWD" open -n -b "com.microsoft.VSCode" --args $*;
-}
-
 subl_symlink() {
   if [ "$(uname)" = "Darwin" ]; then
     ST_APP=$(osascript -e 'POSIX path of (path to application "Sublime Text")')
