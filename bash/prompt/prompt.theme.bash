178e9846291a2ada493a805c241ffa21bc7e8c94
Allen Bargi <allen.bargi@gmail.com>
2016-04-06 21:17:30 +0200

    added nvm version to the prompt

--- bash/prompt/prompt.theme.bash
+++ bash/prompt/prompt.theme.bash
@@ -5,17 +5,30 @@ SCM_THEME_PROMPT_CLEAN=" ${custom_green}✔"
 SCM_THEME_PROMPT_PREFIX=" "
 SCM_THEME_PROMPT_SUFFIX=" "
 SCM_NONE_CHAR=""
-RVM_THEME_PROMPT_PREFIX=" ${custom_red}⟐ "
+RVM_THEME_PROMPT_PREFIX=" ${custom_dark_red}⬣ "
 RVM_THEME_PROMPT_SUFFIX=" ${reset_color}"
+NVM_THEME_PROMPT_PREFIX=" ${green}⬢ "
+NVM_THEME_PROMPT_SUFFIX=" ${reset_color}"
 ERROR="${bold_red}❌  Error \n${normal}"
 EXIT_STATUS="if [ \$? = 0 ]; then echo \"\"; else echo \"${ERROR}\"; fi"
 
 rvmrc_version_prompt () {
   if [ -f .rvmrc ] || [ -f .ruby\-version ] || [ -f Gemfile ]; then
-    if which rvm &> /dev/null; then
-      rvm=$(rvm tools identifier) || return
+    if type rvm &> /dev/null; then
+      shell_session_update() { :; }
+      rvm=`rvm tools identifier`
       clean=${rvm//ruby-/}
       echo -e "$RVM_THEME_PROMPT_PREFIX$clean$RVM_THEME_PROMPT_SUFFIX"
+    fi
+  fi
+}
+
+nvmrc_version_prompt () {
+  if [ -f .nvmrc ]; then
+    if type nvm &> /dev/null; then
+      rvm=`nvm current`
+      clean=${rvm//v/}
+      echo -e "$NVM_THEME_PROMPT_PREFIX$clean$NVM_THEME_PROMPT_SUFFIX"
     fi
   fi
 }
@@ -24,12 +37,12 @@ aws_saml_prompt () {
   local now=$( date -u +%s )
   local remain=$(( EXPIRE - now ))
   if [ -n "$AWS_ROLE" ] && [ -n "$EXPIRETIME" ] && [ $remain -gt 0 ]; then
-    echo -e " ${custom_cyan}[${AWS_ROLE} $(($remain/60))m]${reset_color} "
+    echo -e " ${custom_cyan}[${AWS_ROLE} $(($remain/60))m]${reset_color} "ⓝ
   fi
 }
 
 prompt_left () {
-  LEFT_PROMPT="\[\033[G\]\`${EXIT_STATUS}\`\[${yellow}\]\w\[\$(aws_saml_prompt)\]\[\$(rvmrc_version_prompt)\]\[${custom_blue}\]\[\$(scm_char)\]\[\$(scm_prompt_info)\]\[${normal}\]\[${custom_gray}\]\[$(date "+%_I:%M:%S %P")\]"
+  LEFT_PROMPT="\[\033[G\]\`${EXIT_STATUS}\`\[${yellow}\]\w\[\$(aws_saml_prompt)\]\[\$(nvmrc_version_prompt)\]\[\$(rvmrc_version_prompt)\]\[${custom_blue}\]\[\$(scm_char)\]\[\$(scm_prompt_info)\]\[${normal}\]\[${custom_gray}\]\[$(date "+%_I:%M:%S %P")\]"
   echo -e "${LEFT_PROMPT}"
 }
 
