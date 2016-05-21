545fa86bdfaa704f2dbcb0d66e79e342078e884c
Allen Bargi <allen.bargi@gmail.com>
2016-05-22 01:37:51 +0200

    improved font list and font installer bash script
    - ignored white space and comments

--- templates/provisioning/mac/fonts.sh
+++ templates/provisioning/mac/fonts.sh
@@ -2,4 +2,6 @@
 
 echo "Installing Fonts"
 echo "================"
-while read in; do brew cask install "$in"; done < "$HOME"/.templates/dependencies/fonts
+while read in; do
+  brew cask install "$in"
+done < <(grep -v -E '^$|^#' "$HOME"/.templates/dependencies/fonts)
