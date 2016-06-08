474084fd39aeecd764ce4278b80549adde68a37b
Allen Bargi <allen.bargi@gmail.com>
2016-06-08 13:24:50 +0200

    code should run before installing extensions

--- templates/provisioning/vscode.sh
+++ templates/provisioning/vscode.sh
@@ -2,5 +2,5 @@
 
 echo "Installing VS-Code Extensions"
 echo "====================================="
-
+code
 while read -r in; do code --install-extension "$in"; done < "$HOME/.templates/dependencies/vscode"
