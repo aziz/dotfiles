6ee545f78f22a93801d89cb2eb1e75369a1d9c3a
Allen Bargi <allen.bargi@gmail.com>
2016-08-11 13:35:20 +0200

    more npm aliases

---
+++ bash/aliases/npm.aliases.bash
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+#
+alias npmg="npm -g"
+alias npmgo="npm -g outdated"
+alias npmgo0='npm -g outdated --depth=0'
+alias npmo0='npm outdated --depth=0'
+
+alias ni="npm install"
+alias nis="npm install --save"
+alias nisd="npm install --save-dev"
+
+alias nt="npm test"
+alias nr="npm run"
--- bash/plugins/node.plugin.bash
+++ bash/plugins/node.plugin.bash
@@ -5,16 +5,7 @@ export NODE_REPL_HISTORY="$HOME/.node_history"
 export NODE_REPL_HISTORY_SIZE='32768'
 
 export NVM_DIR="$HOME/.nvm"
-source $(brew --prefix nvm)/nvm.sh
-
-alias npmg="npm -g"
-alias npmgo="npm -g outdated"
-alias npmgo0='npm -g outdated --depth=0'
-alias npmo0='npm outdated --depth=0'
-
-alias ni="npm install"
-alias nis="npm install --save"
-alias nisd="npm install --save-dev"
+source "$(brew --prefix nvm)"/nvm.sh
 
 npmls() {
   npm ls "$@" | grep "^[└├]" | sed "s/─┬/──/g"
