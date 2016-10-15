9b4393ceb931aae7c89a3105f5480278ba471130
Allen Bargi <allen.bargi@gmail.com>
2016-10-15 23:52:43 +0200

    installing php-version and composer and provisioning them

---
+++ bash/plugins/php.plugin.bash
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+source $(brew --prefix php-version)/php-version.sh && php-version 5
---
+++ templates/provisioning/php.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+brew install php70
+brew unlink php70
+brew install php56
+brew install homebrew/php/composer
+brew install homebrew/php/php-version
+brew install homebrew/php/phplint
