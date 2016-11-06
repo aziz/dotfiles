f549d5307ac85cbc589fbfd592d94364290bb1e4
Allen Bargi <allen.bargi@gmail.com>
2016-11-06 16:48:30 +0100

    update brew aliases

--- bash/aliases/homebrew.aliases.bash
+++ bash/aliases/homebrew.aliases.bash
@@ -3,7 +3,7 @@
 if which brew >/dev/null 2>&1; then
   alias br="brew"
   # alias bro="brew update && brew outdated && cask_report"
-  alias brup="brew upgrade --all && brew cleanup && brew prune && rm -rf $(brew --cache)/*"
+  alias brup="brew upgrade && brew cleanup && brew prune && rm -rf $(brew --cache)/*"
   alias brc="brew cask"
   alias bri="brew install"
   alias brs="brew search"
