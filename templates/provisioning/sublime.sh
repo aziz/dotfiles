935dc73e9f49c97cbea8f79d9860cbc3e956f92a
Allen Bargi <allen.bargi@gmail.com>
2016-12-05 23:08:10 +0100

    disabled sidebar enhancement telemetry

--- templates/provisioning/sublime.sh
+++ templates/provisioning/sublime.sh
@@ -33,3 +33,6 @@ if [ "$(uname)" = "Darwin" ]; then
   ST_APP=$(osascript -e 'POSIX path of (path to application "Sublime Text")')
   ln -sf "$ST_APP/Contents/SharedSupport/bin/subl" /usr/local/bin/subl
 fi
+
+touch "$HOME"/.SideBarEnhancements.optout
+
