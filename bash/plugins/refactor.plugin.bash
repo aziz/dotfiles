10cb4e2fc14db28c40370d7c5449a7008550199b
Allen Bargi <allen.bargi@gmail.com>
2018-06-24 22:08:12 +0200

    cleanup

--- bash/plugins/refactor.plugin.bash
+++ bash/plugins/refactor.plugin.bash
@@ -3,12 +3,12 @@
 refactor_large_files () {
   # Usage:
   # refactor_large_files [path] [file-type]
-  find "$1" -name "$2" | xargs wc -l | sort -nr | head -n20
+  find "$1" -name "$2" | xargs wc -l | sort -nr | head -n50
 }
 
 
 refactor_churn () {
   # Usage:
   # refactor_churn [path_filter]
-  git log --all -M -C --name-only | grep -E "$1" | sort | uniq -c | sort -nr | awk 'BEGIN {print "count,file"} {print $1 "," $2}' | head -n20
+  git log --all -M -C --name-only | grep -E "$1" | sort | uniq -c | sort -nr | awk 'BEGIN {print "count,file"} {print $1 "," $2}' | head -n50
 }
