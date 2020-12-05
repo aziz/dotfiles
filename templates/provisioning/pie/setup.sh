66541c83c6531af1befd05be816ecbefb597275f
Allen Bargi <allen.bargi@gmail.com>
2020-12-06 00:51:46 +0100

    wip

--- templates/provisioning/pie/setup.sh
+++ templates/provisioning/pie/setup.sh
@@ -2,6 +2,8 @@ sudo wget http://goo.gl/vewCLL -O /etc/apt/sources.list.d/rpimonitor.list
 sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get install -y ncdu rpimonitor curl
+# for home assistant
+sudo apt-get install network-manager apparmor-utils
 git clone https://github.com/SensorsIot/IOTstack.git
 curl -sSL https://install.pi-hole.net | bash
 sudo apt autoremove
