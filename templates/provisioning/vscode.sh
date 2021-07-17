cb384099469f7240c1a246f003ab31b0678c5bdd
Allen Bargi <allen.bargi@gmail.com>
2021-07-17 11:29:34 +0200

    cleanup

--- templates/provisioning/vscode.sh
+++ templates/provisioning/vscode.sh
@@ -1,18 +1,14 @@
 #!/usr/bin/env bash
 
-vscode_dotfiles_dir="$HOME/.templates/vscode"
-vscode_target="$HOME/Library/Application Support/Code/User"
+dotfilesDir="$HOME/.templates/vscode"
+target="$HOME/Library/Application Support/Code/User"
 
-for file in $(ls $vscode_dotfiles_dir); do
-  if [ -f "$vscode_target/$file" ]; then
-    rm "$vscode_target/$file"
+for file in $(ls $dotfilesDir); do
+  if [ -f "$target/$file" ]; then
+    rm "$target/$file"
   fi
 done
 
-for file in $vscode_dotfiles_dir/*.json; do
-  ln -s "$file" "$vscode_target"
+for file in $dotfilesDir/*.json; do
+  ln -s "$file" "$target"
 done
-
-echo "Installing VS-Code Extensions"
-echo "====================================="
-while read -r in; do code --install-extension "$in"; done < "$HOME/.templates/dependencies/vscode"
