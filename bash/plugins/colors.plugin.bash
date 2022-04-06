f6360d28552155362b30658142b14c8ea3ffc2ff
Allen Bargi <allen.bargi@gmail.com>
2022-04-06 19:56:14 +0200

    wip

--- bash/plugins/colors.plugin.bash
+++ bash/plugins/colors.plugin.bash
@@ -1,56 +1,5 @@
 #!/usr/bin/env bash
 
-hex2rgb () {
-  hexinput=`echo $1 | tr '[:lower:]' '[:upper:]' | tr -d '#'`
-  a=`echo $hexinput | cut -c-2`
-  b=`echo $hexinput | cut -c3-4`
-  c=`echo $hexinput | cut -c5-6`
-  r=`echo "ibase=16; $a" | bc`
-  g=`echo "ibase=16; $b" | bc`
-  b=`echo "ibase=16; $c" | bc`
-  echo "$r,$g,$b"
-}
-
-rgb2hex () {
-  x=`echo $1 | sed -e 's/,/ /g'`
-  echo `ruby -pae '$_=?#+"%02X"*3%$F' <<< "$x"`
-}
-
-color () {
-  if [[ $1 ]]; then
-    block="▇▇▇"
-    xterm_color=`python $HOME/.templates/terminal/xterm_color_rgb.py $1`
-    xterm_color_16=`printf '%x\n' $xterm_color`
-    tput_color=`tput setaf $xterm_color`
-    hex_color=`rgb2hex $1`
-    echo -e "COLOR\nRGB     \nHEX   \nXTERM\nXTERM16\n$tput_color$block\n$1\n$hex_color\n$xterm_color\n$xterm_color_16" | paste - - - - -
-  else
-    echo "missing argument" >&2
-    cat <<usage
-Preview RGB colors in terminal
-Usage:
-  $ color 255,58,131
-  $ color '255,58,131'
-usage
-    return 1
-  fi
-}
-
-color_hex () {
-  if [[ $1 ]]; then
-    color <<< echo `hex2rgb "$1"`
-  else
-    echo "missing argument" >&2
-    cat <<usage
-Preview HEX colors in terminal
-Usage:
-  $ color_hex 4EE161
-  $ color_hex '#4EE161'
-usage
-    return 1
-  fi
-}
-
 colors () {
   if [ $# -eq 0 ]; then
     T='Color'
@@ -79,11 +28,6 @@ colors () {
   echo
 }
 
-colors256 () {
-  echo "USAGE: "
-  echo "echo -e \`tput setaf \$((16#CODE));\` SAMPLE TEXT"
-  python "$HOME"/.templates/terminal/xterm_color_chart.py -nr -x 3 -y 2
-}
 
 colors_custom () {
   block="▇▇ "
