9259fc4739f45058285ed06e430bb7515f78e1be
Allen Bargi <allen.bargi@gmail.com>
2024-01-03 15:09:09 +0100

    WIP

--- templates/provisioning/pie/setup.sh
+++ templates/provisioning/pie/setup.sh
@@ -1,15 +1,31 @@
 sudo apt-get update
 sudo apt-get upgrade
-sudo apt-get install dirmngr
-sudo apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 2C0D3C0F
-sudo wget https://raw.githubusercontent.com/XavierBerger/RPi-Monitor/master/src/etc/apt/sources.list.d/rpimonitor.list -O /etc/apt/sources.list.d/rpimonitor.list
-sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get install -y ncdu rpimonitor curl git
-# for home assistant
-sudo apt-get install network-manager apparmor-utils
-git clone https://github.com/SensorsIot/IOTstack.git
+sudo apt full-upgrade
+
+# install Cockpit and git
+sudo apt-get install -y git cockpit nala
+
+# install pihole
 curl -sSL https://install.pi-hole.net | bash
+
+# install IOTstack
+git clone https://github.com/SensorsIot/IOTstack.git
+
+# Install Homebridge
+curl -sSfL https://repo.homebridge.io/KEY.gpg | sudo gpg --dearmor | sudo tee /usr/share/keyrings/homebridge.gpg  > /dev/null
+echo "deb [signed-by=/usr/share/keyrings/homebridge.gpg] https://repo.homebridge.io stable main" | sudo tee /etc/apt/sources.list.d/homebridge.list > /dev/null
+sudo apt-get update
+sudo apt-get install homebridge
+
+# Clean up
+sudo apt clean
 sudo apt autoremove
 sudo raspi-config
 sudo reboot
+
+# Add these to .profile
+alias cls="clear"
+alias c="clear"
+alias u="cd .."
+alias w="which"
+alias reload="source ~/.profile"
