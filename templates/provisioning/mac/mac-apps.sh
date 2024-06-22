cacd84ca685c1a72096ac8371b2df8d2c6c95593
Allen Bargi <allen.bargi@gmail.com>
2024-06-22 23:29:49 +0200

    wip

--- templates/provisioning/mac/mac-apps.sh
+++ templates/provisioning/mac/mac-apps.sh
@@ -2,4 +2,8 @@
 
 echo "Installing Apps"
 echo "==============="
-brew install --cask --force $(cat "$HOME"/.templates/dependencies/apps|grep -v "#")
+brew install --cask --force $(cat "$HOME"/.templates/dependencies/apps | grep -v "#")
+
+echo "Installing Browsers"
+echo "==================="
+brew install --cask --force $(cat "$HOME"/.templates/dependencies/apps_browsers | grep -v "#")
